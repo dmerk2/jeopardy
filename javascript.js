c7aa258ca0062d54b6b777eccb06c8e31b651643
Daniel Merkin <dan.merkin@gmail.com>
2016-05-25 12:56:26 -0400

    cant figure out terminal....

--- javascript.js
+++ javascript.js
@@ -1,3 +1,66 @@
+<<<<<<< HEAD
+//When the document loads this function will happen
+$(document).ready(function(){
+
+	//when you click q1 the function below will happen
+	$('#q1').on("click", function(){
+		//q1 will say this when clicked on
+		$("#q1").html("<h4><div id='test'>Coolest kid in class</div></h4>");
+		//when click it removes the above event
+		$(this).off('click')
+	//when clicked the function below will happen	
+	$('#test').on("click", function(){
+		//q1 will say this when clicked again
+		$("#q1").html("<h4>Who is Dan</h4>");
+		});
+	});
+
+	$('#q2').on("click", function(){
+		$("#q2").html("<h4><div id='test'>Matt Kim</div></h4>");
+		$(this).off('click')
+	$('#test').on("click", function(){
+		$("#q2").html("<h4>Who is the Tues/Thurs teacher</h4>");
+		});
+	});
+
+	$('#q3').on("click", function(){
+		$("#q3").html("<h4><div id='test'>What does CSS stand for</div></h4>");
+		$(this).off('click')
+	$('#test').on("click", function(){
+		$("#q3").html("<h4>Cascading Style Sheet</h4>");
+		});
+	});
+
+	$('#q4').on('click',function(){
+		$('#q4').html("<h4><div id = 'test'>What does HTML stand for</div></h4>");
+		$(this).off('click')
+	$('#test').on('click',function(){
+		$('#q4').html('<h4>Hypertext Markup Language</h4>');
+		});
+	});
+
+	$('#q5').on('click', function(){
+		$('#q5').html("<h4><div id = 'test'>What is Javascript</h4>");
+		$(this).off('click');
+	$('#test').on('click',function(){
+		$('#q5').html("<h4>A way to make a website interactive</h4>");
+		});
+	});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+=======
 $(document).ready(function(){
 
 // on('.click', function() {
@@ -9,3 +72,4 @@ $(document).ready(function(){
 		});
 	});
 });
+>>>>>>> 1ccd7d67092a4175f652d0ca3917a85e594cabe5
